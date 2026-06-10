268b660b6b909a04b525430e268b1d364c7c20ba
Ran Ezra <rane@wix.com>
2026-06-10 08:20:31 +0000

    File changes

--- src/components/RotatingBadgeOpenHouse.jsx
+++ src/components/RotatingBadgeOpenHouse.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 export default function RotatingBadgeOpenHouse() {
   return (
-    <div className="absolute top-2 right-2 md:top-4 md:right-4 w-[68px] h-[68px] md:w-[95px] md:h-[95px] pointer-events-none z-10">
+    <div className="absolute top-1 right-1 md:top-2 md:right-2 w-[68px] h-[68px] md:w-[95px] md:h-[95px] pointer-events-none z-10">
       <motion.svg
         viewBox="0 0 100 100"
         className="w-full h-full"
