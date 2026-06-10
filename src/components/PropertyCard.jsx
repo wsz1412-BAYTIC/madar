a502664a75e9b76024f0dffad19d00d609675422
Ran Ezra <rane@wix.com>
2026-06-10 07:20:17 +0000

    File changes

--- src/components/PropertyCard.jsx
+++ src/components/PropertyCard.jsx
@@ -23,7 +23,7 @@ export default function PropertyCard({ property, size = "default" }) {
         viewport={{ once: true, margin: "-50px" }}
         transition={{ duration: 0.6 }}>
         
-        <div className="relative overflow-hidden aspect-square md:aspect-[16/9]">
+        <div className="relative overflow-hidden aspect-square">
           <img
             src={property.featured_image}
             alt={property.title}
